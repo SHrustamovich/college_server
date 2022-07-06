75a866e7d22512db6ca11d22712443f015dd070e
SHrustamovich <87068197+SHrustamovich@users.noreply.github.com>
2022-07-07 00:57:38 +0500

    iii

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 const connection = {
-    connectionString: 'postgres://nmfvmito:6FljDRSUyMzNxnbJHDwU8ldiK_TWNZNy@hattie.db.elephantsql.com/nmfvmito',
-    connectionElString:''
+    connectionString: 'postgres://nmfvmito:6FljDRSUyMzNxnbJHDwU8ldiK_TWNZNy@hattie.db.elephantsql.com/nmfvmito'
 }
 const PORT = process.env.port || 9000
 const SECRET_KEY = "NO_SECRET_KEY"
