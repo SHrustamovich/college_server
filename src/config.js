dde0e99d01316412b7a36e6d831b5362b95af0c8
SHrustamovich <87068197+SHrustamovich@users.noreply.github.com>
2022-07-07 01:03:05 +0500

    s

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const connection = {
     connectionString: 'postgres://nmfvmito:6FljDRSUyMzNxnbJHDwU8ldiK_TWNZNy@hattie.db.elephantsql.com/nmfvmito'
 }
-const PORT = process.env.port || 9000
+const PORT = process.env.PORT || 9000
 const SECRET_KEY = "NO_SECRET_KEY"
 module.exports = {
     connection,
